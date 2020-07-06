a7c3f7df0de7d019a36b9e7b2cc27d7985494c5d
Lar-nova <larinova13@gmail.com>
2020-07-06 13:23:17 +0300

    Added and configured webpack, separated the logic from the view, added an image to the background and made its blur

--- src/views/Gallery/Gallery.tsx
+++ src/views/Gallery/Gallery.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import firebase from "firebase";
-import '../../App.css';
-import styled from "styled-components";
-import CardTypes from "../../components/CardTypes";
 import {DocumentData, QueryDocumentSnapshot, QuerySnapshot} from "@firebase/firestore-types";
+import GalleryView from "./GalleryView";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyAEybk5IYJbjkh0m_1dMEKZ2cfQsumpdh0",
@@ -19,7 +17,7 @@ const firebaseConfig = {
 const IMAGE_TYP = 1;
 const IMAGE = 0;
 
-const Gallery = () => {
+const GalleryController = () => {
 
 	const [url, setUrl] = useState();
 	const [imagesList, setImages] = useState<QueryDocumentSnapshot[][]>();
@@ -51,58 +49,9 @@ const Gallery = () => {
 
 	}, []);
 
-	const renderBody = () => {
-		if (imagesList) {
-			return imagesList.map((imageArray) => (
-				<CardContainer>
-					<CardTypes url={imageArray[0].data().url} type={imageArray[0].data().img_type} handleClick={(type) => {
-						alert(type);
-					}}/>
-				</CardContainer>
-			))
-		}
-		return null;
-	};
-
 	return (
-		<MainComponent>
-			<ScrollView>
-				<Row>
-					{renderBody()}
-				</Row>
-			</ScrollView>
-		</MainComponent>
+		<GalleryView imagesList={imagesList} />
 	);
 };
 
-const MainComponent = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background: #61dafb;
-`;
-
-const ScrollView = styled.div`
-  margin: 0 auto;
-  height: 100vh;
-  width: 100vw;
-  overflow: auto;
-  display: flex;
-  align-items: center; 
-  flex-direction: column;
-`;
-
-const Row = styled.div`
-	width: 1400px;
-  display: grid;
-  grid-template-columns: repeat(3, 33.3%);
-  @media (max-width: 1400px) { 
-  	width: 100%;
-  }
-`;
-
-const CardContainer = styled.div`
-	padding: 10px;
-`;
-
-
-export default Gallery;
+export default GalleryController;
